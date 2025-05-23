e450fe572bf783575de1288df00bf7001731940d
Rianzinhu <rianrochaaraujo1@gmail.com>
2025-05-23 19:32:52 -0300

    DiceRoller

---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,34 @@
+﻿
+namespace DiceRoller
+{
+    public class Dice
+    {
+        //Atributos - utilizamos property nesse caso. (public tipo Nome)
+        public int FaceParaBaixo;
+        public int FaceParaCima;
+        public int QuantidadeDeFaces;
+
+        public Dice()
+        {
+            QuantidadeDeFaces = 6;
+        }
+        public Dice(int faces)
+        {
+            QuantidadeDeFaces = faces;
+        }
+
+        //Método responsável por sortear um número
+        public void Sortear()
+        {
+            //Gero um numero aleatório de 1 a 6
+            Random random = new Random();
+            //Jogo esse número na face para cima
+            FaceParaCima = random.Next(1,QuantidadeDeFaces + 1);
+            // A soma mágica = o menor lado possível + o maior
+            // A soma mágica = 1 + quantidadeDeFaces
+            FaceParaBaixo = QuantidadeDeFaces + 1 - FaceParaCima;
+            //Calcular a face para baixo
+        }
+
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace DiceRoller
+{
+    public partial class MainPage : ContentPage
+    {
+        Game jogo = new Game();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void FlipDice_Clicked(object sender, EventArgs e)
+        {
+            Dice dado = new dado();
+            DadoImage.Source = dado.Flip() + ".png";
+
+            int sorteio = dado.LadoSorteado == "1" ? 0 : 1;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
